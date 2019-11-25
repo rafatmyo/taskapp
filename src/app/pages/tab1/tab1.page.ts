0e3a3ad2f3dc751908e90bfe87f0ff25f95b224e
rafatmyo <rafatmyo@gmail.com>
2019-11-25 01:22:15 -0600

    Added: Item list add

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -41,14 +41,14 @@ export class Tab1Page {
                         if (data.titulo.length === 0){
                             return;
                         }
-                        this.deseosService.crearLista(data.titulo)
+                        const listaId = this.deseosService.crearLista(data.titulo)
+                        this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
                     }
                 }
             ]
           });
       
           await alert.present();
-        //this.router.navigateByUrl('/tabs/tab1/agregar');
     }
 
 }
