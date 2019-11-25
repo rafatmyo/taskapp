c636925f79417358ec49e3925ca274b0f1dc4f0e
rafatmyo <rafatmyo@gmail.com>
2019-11-24 23:02:59 -0600

    Added: List titles to LocalStorage

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DeseosService } from 'src/app/services/deseos.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    
+    constructor(public deseosService: DeseosService,
+                private router: Router,
+                private alertCtrl: AlertController) {
+        
+    }
 
-  constructor() {}
+    async agregarLista(){
+        const alert = await this.alertCtrl.create({
+            header: 'Nueva lista',
+            inputs: [
+                {
+                    name:'titulo',
+                    type: 'text',
+                    placeholder: 'Nombre de la lista'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancelar');
+                    }
+                },
+                {
+                    text: 'Crear',
+                    handler: (data) => {
+                        console.log(data);
+                        if (data.titulo.length === 0){
+                            return;
+                        }
+                        this.deseosService.crearLista(data.titulo)
+                    }
+                }
+            ]
+          });
+      
+          await alert.present();
+        //this.router.navigateByUrl('/tabs/tab1/agregar');
+    }
 
 }
